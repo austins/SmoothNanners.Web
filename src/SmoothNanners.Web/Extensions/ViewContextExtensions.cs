0b6998eb137a7c2ea1ad78ccf3aa675ea7d55703
austins <austins@users.noreply.github.com>
2024-10-23 12:39:58 -0700

    Make local script dedupe work in partial views

---
+++ src/SmoothNanners.Web/Extensions/ViewContextExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SmoothNanners.Web.Extensions;
+
+internal static class ViewContextExtensions
+{
+    public const string LocalPathPrefix = "~/";
+
+    public static IReadOnlyList<string> GetScripts(this ViewContext viewContext)
+    {
+        return GetOrInitializeScripts(viewContext);
+    }
+
+    public static bool TryAddLocalScript(this ViewContext viewContext, string path)
+    {
+        if (!path.StartsWith(LocalPathPrefix, StringComparison.Ordinal))
+        {
+            throw new ArgumentException(
+                $"Script {nameof(path)} must be a local path starting with '~/'.",
+                nameof(path));
+        }
+
+        var scripts = GetOrInitializeScripts(viewContext);
+        if (!scripts.Contains(path))
+        {
+            scripts.Add(path);
+            return true;
+        }
+
+        return false;
+    }
+
+    private static List<string> GetOrInitializeScripts(ViewContext viewContext)
+    {
+        const string scriptsKey = "ViewScripts";
+        viewContext.HttpContext.Items.TryAdd(scriptsKey, new List<string>());
+        return (viewContext.HttpContext.Items[scriptsKey] as List<string>)!;
+    }
+}
--- src/SmoothNanners.Web/Extensions/ViewDataExtensions.cs
+++ src/SmoothNanners.Web/Extensions/ViewDataExtensions.cs
@@ -5,7 +5,6 @@ namespace SmoothNanners.Web.Extensions;
 internal static class ViewDataExtensions
 {
     private const string TitleKey = "Title";
-    private const string ScriptsKey = "Scripts";
 
     public static string? GetTitle(this ViewDataDictionary viewData)
     {
@@ -15,33 +14,5 @@ internal static class ViewDataExtensions
     public static void SetTitle(this ViewDataDictionary viewData, string title)
     {
         viewData[TitleKey] = title;
-    }
-
-    public static IReadOnlyDictionary<string, bool> GetScripts(this ViewDataDictionary viewData)
-    {
-        return GetOrInitializeScripts(viewData);
-    }
-
-    public static bool TryAddLocalScript(this ViewDataDictionary viewData, string src, bool defer = false)
-    {
-        // "~/" is needed to get correct path for static assets.
-        if (!src.StartsWith("~/", StringComparison.Ordinal))
-        {
-            throw new ArgumentException($"Script {nameof(src)} must be a local path.", nameof(src));
-        }
-
-        return GetOrInitializeScripts(viewData).TryAdd(src, defer);
-    }
-
-    private static OrderedDictionary<string, bool> GetOrInitializeScripts(ViewDataDictionary viewData)
-    {
-        if (viewData.TryGetValue(ScriptsKey, out var value) && value is OrderedDictionary<string, bool> existingScripts)
-        {
-            return existingScripts;
-        }
-
-        var newScripts = new OrderedDictionary<string, bool>();
-        viewData[ScriptsKey] = newScripts;
-        return newScripts;
     }
 }

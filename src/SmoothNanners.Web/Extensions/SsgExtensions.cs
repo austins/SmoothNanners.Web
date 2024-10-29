62685237b47bce0e8c6c44d10f9a79205e9fc42a
austins <austins@users.noreply.github.com>
2024-10-29 01:15:25 -0700

    Revert "Copy precompressed gz and br assets to output artifact"
    This reverts commit 92e3364f186eb1b942c1963eacdb4fcff34698e5.

--- src/SmoothNanners.Web/Extensions/SsgExtensions.cs
+++ src/SmoothNanners.Web/Extensions/SsgExtensions.cs
@@ -6,7 +6,6 @@ internal static class SsgExtensions
 {
     public static IServiceCollection AddSsg(this IServiceCollection services)
     {
-        // Specify pages to prerender.
         return services.AddSingleton<IStaticResourcesInfoProvider>(
             new StaticResourcesInfoProvider(
             [
@@ -15,14 +14,22 @@ internal static class SsgExtensions
                 {
                     Query = "?code=404",
                     OutFile = "404.html"
-                }
+                },
+                new CssResource("/assets/vendors/bootswatch/dist/vapor/bootstrap.min.css")
+                {
+                    OptimizerType = OptimizerType.None
+                },
+                new CssResource("/assets/styles/main.css"),
+                new JsResource("/assets/scripts/youtube-embed.js"),
+                new BinResource("/favicon.ico") { OptimizerType = OptimizerType.None },
+                new BinResource("/assets/images/avatar.jpg") { OptimizerType = OptimizerType.None }
             ]));
     }
 
     public static void RunSsgAndExit(this WebApplication app)
     {
         // Ensure output directory exists.
-        var ssgOutputPath = Path.Combine(app.Environment.ContentRootPath, "ssg");
+        var ssgOutputPath = Path.Combine(app.Environment.ContentRootPath, "bin/ssg");
         if (Directory.Exists(ssgOutputPath))
         {
             Directory.Delete(ssgOutputPath, true);
@@ -34,21 +41,6 @@ internal static class SsgExtensions
         app.Logger.LogInformation("Running SSG and exiting...");
 #pragma warning restore CA1848
 
-        CopyWebRootContents(app.Environment.WebRootPath, ssgOutputPath);
-
         app.GenerateStaticContent(ssgOutputPath, true);
-    }
-
-    private static void CopyWebRootContents(string webRootPath, string ssgOutputPath)
-    {
-        foreach (var directoryPath in Directory.GetDirectories(webRootPath, "*", SearchOption.AllDirectories))
-        {
-            Directory.CreateDirectory(directoryPath.Replace(webRootPath, ssgOutputPath, StringComparison.Ordinal));
-        }
-
-        foreach (var filePath in Directory.GetFiles(webRootPath, "*.*", SearchOption.AllDirectories))
-        {
-            File.Copy(filePath, filePath.Replace(webRootPath, ssgOutputPath, StringComparison.Ordinal));
-        }
     }
 }

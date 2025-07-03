df2087c88c30e7dbb25f1e0cc21f1f6faf251693
austins <austins@users.noreply.github.com>
2025-07-03 15:07:55 -0700

    Refactor `ViewDataExtensions` to use `TryGetTitle` for improved clarity and null handling

--- src/SmoothNanners.Web/Extensions/ViewDataExtensions.cs
+++ src/SmoothNanners.Web/Extensions/ViewDataExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SmoothNanners.Web.Extensions;
 
@@ -6,9 +7,16 @@ internal static class ViewDataExtensions
 {
     private const string TitleKey = "Title";
 
-    public static string? GetTitle(this ViewDataDictionary viewData)
+    public static bool TryGetTitle(this ViewDataDictionary viewData, [NotNullWhen(true)] out string? title)
     {
-        return viewData[TitleKey] is string title && !string.IsNullOrWhiteSpace(title) ? title : null;
+        if (viewData[TitleKey] is string titleValue && !string.IsNullOrWhiteSpace(titleValue))
+        {
+            title = titleValue;
+            return true;
+        }
+
+        title = null;
+        return false;
     }
 
     public static string SetTitle(this ViewDataDictionary viewData, string title)

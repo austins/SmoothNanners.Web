3c88b59822c1dfbb7d98b0f18e27bf3f610fe2f0
austins <1623983+austins@users.noreply.github.com>
2026-02-17 11:05:48 -0800

    Use CSS isolation bundle

--- src/SmoothNanners.Web/Program.cs
+++ src/SmoothNanners.Web/Program.cs
@@ -1,5 +1,6 @@
 using AspNetStatic;
 using AspNetStatic.Optimizer;
+using System.Reflection;
 
 var isSsg = args.HasExitWhenDoneArg();
 
@@ -21,6 +22,10 @@ builder.Services.Configure<RouteOptions>(o =>
     o.LowercaseQueryStrings = true;
 });
 
+// Allow the app's isolated CSS bundle to be produced as a static asset in environments other than Development as SSG is done on the built app instead of a published build.
+// We use asp-append-version in place of static asset fingerprinting in order to have fixed paths to fetch for SSG.
+builder.WebHost.UseStaticWebAssets();
+
 if (isSsg)
 {
     IReadOnlyList<NonPageResource> assetResources =
@@ -28,7 +33,9 @@ if (isSsg)
         new BinResource("/favicon.ico"),
         new CssResource("/assets/vendors/bootswatch/dist/vapor/bootstrap.min.css")
             { OptimizationType = OptimizationType.None },
-        new CssResource("/assets/app.css") { OptimizationType = OptimizationType.Css },
+        new CssResource("/assets/global.css") { OptimizationType = OptimizationType.Css },
+        new CssResource($"/{Assembly.GetExecutingAssembly().GetName().Name}.styles.css")
+            { OptimizationType = OptimizationType.Css },
         new JsResource("/assets/vendors/alpinejs/dist/cdn.min.js") { OptimizationType = OptimizationType.None },
         new BinResource("/assets/images/avatar.webp")
     ];

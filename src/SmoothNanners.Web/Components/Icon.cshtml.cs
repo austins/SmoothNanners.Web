16a996222f3dbfe7fc39bd203035f4aa89e0da9f
austins <1623983+austins@users.noreply.github.com>
2026-05-25 05:02:30 -0700

    Add HtmlTargetElement attribute to Icon component

--- src/SmoothNanners.Web/Components/Icon.cshtml.cs
+++ src/SmoothNanners.Web/Components/Icon.cshtml.cs
@@ -1,8 +1,10 @@
 using Blazicons;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using TechGems.StaticComponents;
 
 namespace SmoothNanners.Web.Components;
 
+[HtmlTargetElement("icon", TagStructure = TagStructure.WithoutEndTag)]
 public sealed class Icon : StaticNode
 {
     public SvgIcon SvgIcon { get; set; } = null!;
